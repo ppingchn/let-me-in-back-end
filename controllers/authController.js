fb80369eeaf41abe3530e1afc13e4d5b79056e96
Saowaluckpruitt <102138233+Saowaluckpruitt@users.noreply.github.com>
2022-06-27 11:36:59 +0700

    Controllers (#3)
    * table User done
    
    * user register  fix
    
    * register done
    
    * fix username  and fix maxupload at profilepic can  be 1 pic
    
    * login done
    
    * Update controllers/authController.js
    
    * Update controllers/authController.js
    
    Co-authored-by: dolphinza8 <dolphinza8nj@gmail.com>
    Co-authored-by: ppingchn <82163049+ppingchn@users.noreply.github.com>

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,9 +2,17 @@ const validator = require("validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-const createError = require("../utils/createError");
-const { User } = require("../models");
-const sequelize = require("sequelize");
+const createError = require("../util/createError");
+const {
+  User,
+  Education,
+  Skill,
+  Experience,
+  CompanyDetail,
+} = require("../models");
+const { sequelize } = require("../models");
+const fs = require("fs");
+const cloudinary = require("../util/cloundinary");
 
 //CREATE TOKEN
 const createToken = (payload) =>
@@ -14,9 +22,9 @@ const createToken = (payload) =>
 
 exports.login = async (req, res, next) => {
   try {
-    const { userName, password } = req.body;
+    const { username, password } = req.body;
     const user = await User.findOne({
-      where: { userName },
+      where: { username: username },
     });
 
     if (!user) {
@@ -37,46 +45,125 @@ exports.login = async (req, res, next) => {
 
 exports.register = async (req, res, next) => {
   try {
-    const { userName, phoneNumber, password, confirmPassword } = req.body;
+    const result = await sequelize.transaction(async (t) => {
+      const {
+        username,
+        password,
+        confirmPassword,
+        role,
+        email,
+        phoneNumber,
+        educationArray,
+        experienceArray,
+        skillArray,
+        companyName,
+        websiteLink,
+        overView,
+        address,
+        location,
+      } = req.body;
 
-    if (!phoneNumber) {
-      createError("phone number is require", 400);
-    }
-    if (!password) {
-      createError("password is require", 400);
-    }
-    if (password !== confirmPassword) {
-      createError("password did not match", 400);
-    }
+      const stockPic = {};
 
-    const isPhoneNumber = validator.isMobilePhone(phoneNumber + "");
-    if (!isPhoneNumber) {
-      createError("Invalid phone number", 400);
-    }
+      if (req.files?.profilePic) {
+        const result = await cloudinary.upload(req.files.profilePic[0].path);
+        stockPic.profilePic = result.secure_url;
+      }
+      if (req.files?.coverPic) {
+        const result = await cloudinary.upload(req.files.coverPic[0].path);
+        stockPic.coverPic = result.secure_url;
+      }
 
-    const hashedPassword = await bcrypt.hash(password, 12);
-    const user = await User.create({
-      userName,
-      phoneNumber,
-      password: hashedPassword,
+      if (!phoneNumber) {
+        createError("phone number is require", 400);
+      }
+      if (!password) {
+        createError("password is require", 400);
+      }
+      if (password !== confirmPassword) {
+        createError("password did not match", 400);
+      }
+
+      // Make sure they gave use a valid phone number
+      const isPhoneNumber = validator.isMobilePhone(phoneNumber + "");
+      if (!isPhoneNumber) {
+        createError("Invalid phone number", 400);
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 12);
+
+      if (role === "user" || role === "company") {
+        const user = await User.create({
+          username,
+          profilePic: stockPic.profilePic,
+          coverPic: stockPic.coverPic,
+
+          role,
+          email,
+          phoneNumber,
+          password: hashedPassword,
+        });
+
+        //create Education
+        if (role === "user") {
+          const eduArray = JSON.parse(educationArray);
+
+          eduArray.map(
+            async (el) =>
+              await Education.create({
+                userId: user.id,
+                degree: el.degree,
+                university: el.university,
+                field: el.field,
+                yearStart: el.yearStart,
+                yearEnd: el.yearEnd,
+              })
+          );
+
+          const experience = JSON.parse(experienceArray);
+
+          experience.map(
+            async (el) =>
+              await Experience.create({
+                companyName: el.companyName,
+                position: el.position,
+                yearStart: el.yearStart,
+                yearEnd: el.yearEnd,
+                userId: user.id,
+              })
+          );
+
+          const skill = JSON.parse(skillArray);
+
+          skill.map(
+            async (el) =>
+              await Skill.create({
+                title: el.title,
+                userId: user.id,
+              })
+          );
+        } else if (role === "company") {
+          await CompanyDetail.create({
+            companyName,
+            websiteLink,
+            overView,
+            address,
+            location,
+            userId: user.id,
+          });
+        }
+      }
     });
-
-    const token = createToken({ id: user.id });
+    const token = createToken({ id: User.id });
     res.status(201).json({ token });
   } catch (err) {
     next(err);
-  }
-};
-exports.companyRegister = async (req, res, next) => {
-  try {
-  } catch (err) {
-    next(err);
-  }
-};
-
-exports.userRegister = async (req, res, next) => {
-  try {
-  } catch (err) {
-    next(err);
+  } finally {
+    if (req.files?.profilePic) {
+      fs.unlinkSync(req.files.profilePic[0].path);
+    }
+    if (req.files?.coverPic) {
+      fs.unlinkSync(req.files.coverPic[0].path);
+    }
   }
 };
