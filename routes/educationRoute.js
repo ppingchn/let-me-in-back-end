c8a2f3195f6ee064c89cf5f4d15d76ad95eecea5
kanm4s <kanm4s@gmail.com>
2022-07-01 13:57:35 +0700

    add upload coverimage

--- routes/educationRoute.js
+++ routes/educationRoute.js
@@ -5,7 +5,7 @@ const educationController = require('../controllers/educationController');
 
 router.get('/:id', educationController.getEducationById);
 router.post('/', educationController.createEducation);
-router.put('/educationId', educationController.updateEducation);
-router.delete('/educattionId', educationController.deleleEducation);
+router.put('/:educationId', educationController.updateEducation);
+router.delete('/:educationId', educationController.deleleEducation);
 
 module.exports = router;
